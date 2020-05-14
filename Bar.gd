83a6a1d17aa959a2232d0b291169445364ac9e01
Alejandro Carrasco <alejandro.carrasco@gmail.com>
2020-05-14 19:37:53 +0200

    add sample image, readme, better code for control

--- Bar.gd
+++ Bar.gd
@@ -5,6 +5,7 @@ export var sway_speed = 100
 
 export var max_rotation = deg2rad(90)
 export var rotation_speed = 4 * deg2rad(360)
+export var minimum_sway = 0.1
 
 export var SWAY_AXIS = [JOY_ANALOG_LY, JOY_ANALOG_RY]
 export var ROTATE_AXIS = [JOY_ANALOG_LX, JOY_ANALOG_RX]
@@ -36,18 +37,24 @@ func deactivate()	:
 	analog = -1
 	$CSGCylinder.material = inactive_material
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	if analog < 0:
-		return
+func move(delta):
 	var sway_input = invert * Input.get_joy_axis(joy, SWAY_AXIS[analog])
+	if abs(sway_input) < minimum_sway:
+		sway_input = 0
 	var sway_target = clamp(sway + sway_input * delta * sway_speed, -max_sway, max_sway)
 	var sway_diff = sway_target - sway
 	sway = sway_target
 	translate(Vector3(0, 0, sway_diff))
-	
+
+func spin(delta):
 	var rotate_input = invert * Input.get_joy_axis(joy, ROTATE_AXIS[analog])
 	var rotate_target = rotate_input * max_rotation
 	var rotate_diff = (rotate_target - rotate) * delta * rotation_speed
 	rotate += rotate_diff
 	rotate_z(rotate_diff)
+
+func _process(delta):
+	if analog < 0:
+		return
+	move(delta)
+	spin(delta)
---
+++ README.md
@@ -0,0 +1,10 @@
+Foosball 3d
+===========
+
+This is a simple project to explore Godot as a game engine.
+
+It is intended as a casual party game, best played with 4 people
+(like real foosball), where each player has their own xbox-like
+controller and control two bars with the analog sticks.
+
+![sample](sample.png)
